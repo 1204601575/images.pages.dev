aeba546a0f7474d9a950d03f4610d04be06e23a6
1204601575 <zhangjiejeff@qq.com>
2021-11-22 10:55:23 +0800

    new
    jew

--- node_modules/@ssttevee/multipart-parser/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Part<TData = Uint8Array> {
-    name: string;
-    data: TData;
-    filename?: string;
-    contentType?: string;
-}
-export declare function streamMultipart(body: ReadableStream<Uint8Array>, boundary: string): AsyncIterableIterator<Part<AsyncIterableIterator<Uint8Array>>>;
-export declare function iterateMultipart(body: ReadableStream<Uint8Array>, boundary: string): AsyncIterableIterator<Part>;
-export declare function parseMultipart(body: ReadableStream<Uint8Array>, boundary: string): Promise<Part[]>;
